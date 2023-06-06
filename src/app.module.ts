9b662ef180827a10ac21a4f6b0ff89cfaa6b3154
SualaDev <81395134+SualaDev@users.noreply.github.com>
2023-06-06 16:37:33 +0100

    project restructure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { NewUserModule } from './users/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/auth'),
-    UserModule,
-    AuthModule,
+    NewUserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
