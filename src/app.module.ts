d6300bad5497d63d0f8d35d53577eae147fc5045
SualaDev <81395134+SualaDev@users.noreply.github.com>
2023-06-19 18:07:14 +0100

    semi-completed project

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NewUserModule } from './users/user.module';
+import { NewOrderModule } from './order/order.module';
+import { NewAuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/auth'),
+    // MongooseModule.forRoot('mongodb+srv://tamunosiesualaamotsuka:<password>@cluster0.3lnlyqf.mongodb.net/?retryWrites=true&w=majority'),
     NewUserModule,
+    NewOrderModule,
+    NewAuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
