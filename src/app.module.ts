e0340a8649303f56a9b652f5be2c0d8b4fbedc86
SualaDev <81395134+SualaDev@users.noreply.github.com>
2023-06-27 15:41:24 +0100

    program files updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,19 @@ import { NewUserModule } from './users/user.module';
 import { NewOrderModule } from './order/order.module';
 import { NewAuthModule } from './auth/auth.module';
 
+import { ConfigModule } from '@nestjs/config';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+const name = process.env.DATABASE_USERNAME;
+const password = process.env.DATABASE_PASSWORD;
+const dbname = process.env.DATABASE_NAME;
 @Module({
   imports: [
-    // MongooseModule.forRoot('mongodb+srv://tamunosiesualaamotsuka:<password>@cluster0.3lnlyqf.mongodb.net/?retryWrites=true&w=majority'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${name}:${password}@ayoskilolo-cluster.jbphaan.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+    ),
     NewUserModule,
     NewOrderModule,
     NewAuthModule,
