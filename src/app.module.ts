1203633ecf9721a418b12435b1d760cfae4385ff
Ayomide Afolabi <ayoafo82@gmail.com>
2023-07-05 13:11:07 +0100

    Revert "program files updated"
    This reverts commit e0340a8649303f56a9b652f5be2c0d8b4fbedc86.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,9 @@ import { NewUserModule } from './users/user.module';
 import { NewOrderModule } from './order/order.module';
 import { NewAuthModule } from './auth/auth.module';
 
-import { ConfigModule } from '@nestjs/config';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config();
-
-const name = process.env.DATABASE_USERNAME;
-const password = process.env.DATABASE_PASSWORD;
-const dbname = process.env.DATABASE_NAME;
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(
-      `mongodb+srv://${name}:${password}@ayoskilolo-cluster.jbphaan.mongodb.net/${dbname}?retryWrites=true&w=majority`,
-    ),
+    // MongooseModule.forRoot('mongodb+srv://tamunosiesualaamotsuka:<password>@cluster0.3lnlyqf.mongodb.net/?retryWrites=true&w=majority'),
     NewUserModule,
     NewOrderModule,
     NewAuthModule,
